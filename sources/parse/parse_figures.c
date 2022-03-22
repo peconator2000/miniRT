e6a44b328d58e43d29966f0c2bfdcf70cd8f8194
Ramazan Abdulaev <txmi@Ramazans-MacBook-Air.local>
2022-03-22 14:12:40 +0300

    perenes stroku prisvaivania scene into fig union

--- sources/parse/parse_figures.c
+++ sources/parse/parse_figures.c
@@ -6,12 +6,12 @@ void	parse_cylinder(t_figures **figs, char **str, t_scene *scene)
 
 	add_figure(figs, new_figure(CYLINDER));
 	current_fig = get_last_fig(*figs);
+	current_fig->fig.scene = scene;
 	skip_space(str);
 	current_fig->fig.cy.coord = parse_point3(str);
 	current_fig->no_vec = normalize(parse_point3(str));
 	current_fig->fig.cy.diameter = str_to_double(str);
 	current_fig->fig.cy.height = str_to_double(str);
-	current_fig->fig.scene = scene;
 	current_fig->color = parse_color(str);
 }
 
@@ -21,12 +21,11 @@ void	parse_sphere(t_figures **figs, char **str, t_scene *scene)
 
 	add_figure(figs, new_figure(SPHERE));
 	current_fig = get_last_fig(*figs);
+	current_fig->fig.scene = scene;
 	skip_space(str);
 	current_fig->fig.sp.coord = parse_point3(str);
 	current_fig->fig.sp.diameter = str_to_double(str);
 	current_fig->color = parse_color(str);
-	// current_fig->fig.scene = scene;
-	(void)scene;
 }
 
 void	parse_plane(t_figures **figs, char **str, t_scene *scene)
@@ -35,9 +34,9 @@ void	parse_plane(t_figures **figs, char **str, t_scene *scene)
 
 	add_figure(figs, new_figure(PLANE));
 	current_fig = get_last_fig(*figs);
+	current_fig->fig.scene = scene;
 	skip_space(str);
 	current_fig->fig.pl.coord = parse_point3(str);
 	current_fig->no_vec = normalize(parse_point3(str));
-	current_fig->fig.scene = scene;
 	current_fig->color = parse_color(str);
 }
